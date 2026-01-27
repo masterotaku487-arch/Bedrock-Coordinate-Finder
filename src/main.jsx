a895da4bd892fbbe983512246afa0c9e33d6eba1
masterotaku487-arch <masterotaku487@gmail.com>
2026-01-27 17:38:38 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,3 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).createRoot(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
